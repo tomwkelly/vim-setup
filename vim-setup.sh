ff61f021bdf97016a6657e057e0a57e0c1d8fa34
Tom Kelly <tom.kelly@exgo.co.uk>
2020-09-11 10:43:06 +0100

    Updated to add plug

--- vim-setup.sh
+++ vim-setup.sh
@@ -13,5 +13,7 @@ mkdir -p ~/.config/nvim/
 mv ./init.vim ~/.config/nvim/
 mv ./coc-settings.json ~/.config/nvim/
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim --headless +PlugInstall +qa
